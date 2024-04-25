79fc509382775e34bd3de169c67aa99db9e6324f
Rajat <rajatsinha5467@gmail.com>
2024-04-25 12:28:06 +0530

    array

---
+++ DATATYPE.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+
+	int a = 5 ;
+	int b = 10 ;
+
+	int c = 5 + 10 ;
+	printf("your addition is :",c);
+
+	getch();
+
+}
---
+++ LECONE.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{       clrscr();
+	printf("hello World !!\n");
+	printf("Welcome Student's into RW...");
+	printf("\nwe are learning c language...");
+	printf("\nwe are in\tinformation technology...");
+	getch();
+}
